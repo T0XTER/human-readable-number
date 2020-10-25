56f97461b8d1be43b38b654ac71982ade6906a33
T0XTER <tyutyunnik.serg@gmail.com>
2020-10-25 13:56:01 +0200

    feat: implement task functionality to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
+    let arrayOne = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],
+        arrayTwo = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 
+    if (number < 20) {
+        return arrayOne[number];
+    }
+
+    if (number < 100) {
+        return arrayTwo[Math.floor(number / 10) - 2] + (number % 10 !== 0 ? " " + toReadable(number % 10) : "");
+    }
+
+    if (number < 1000) {
+        return arrayOne[Math.floor(number / 100)] + " hundred" + (number % 100 !== 0 ? " " + toReadable(number % 100) : "");
+    }
 }
